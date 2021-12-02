fd5faf368c11cd669f81b08d461fefebcf531227
nzrmm <iamgata.me@gmail.com>
2021-12-03 03:56:36 +0700

    add aos animation

--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -2,7 +2,7 @@
   <div v-if="this.$store.state.isLoading" class="flex justify-center items-center">
     <img src="../assets/loading.svg" alt="">  
   </div>
-  <div v-else class="w-12/12 lg:w-6/12" @click="getQuotes">
+  <div v-else class="w-12/12 lg:w-6/12" @click="getQuotes" data-aos="zoom-in" data-aos-duration="1000">
     <div class="flex flex-wrap space-x-2 justify-center lg:justify-start">
       <span v-for="tag in tags" :key="tag" class="px-4 py-1 mb-2 bg-teal-600 text-white rounded-lg">
         {{ tag }}
@@ -17,6 +17,8 @@
 
 <script>
   import { mapState } from 'vuex';
+  import AOS from 'aos';
+  import 'aos/dist/aos.css';
 
   export default {
     name: 'Quotes',
@@ -31,6 +33,7 @@
       }
     },
     mounted() {
+      AOS.init()
       return this.$store.dispatch('getQuotes')
     },
   }
